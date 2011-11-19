19e181c354c1d239d735863ed38ff77d4a997e40
Arthur Neves <arthurnn@gmail.com>
2011-11-18 22:16:18 -0500

    aaa

--- source/config/uat.py
+++ source/config/uat.py
@@ -1,50 +1,105 @@
 '''
-Created on Nov 18, 2011
-
 @author: arthurnn
 '''
+
+# Django settings for adidasEcrmLookbook project.
+from os.path import abspath, dirname, join
+
+PROJECT_ROOT = abspath(join(dirname(__file__), "../"))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+ANALYTICS_ID = 'UA-25908866-2'
+RECAPTCHA_PUB_KEY = "6LfA08gSAAAAALDg2e5xIbCujNbNDFD5EATGYGpr"
+RECAPTCHA_PRIVATE_KEY = "6LfA08gSAAAAALObXgOJg97Aye6lU93bemuh8lmS"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'picturme',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'uik9BrYBXkd',                  # Not used with sqlite3.
-        'HOST': '10.208.210.196',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'adidaslookbook',                      # Or path to database file if using sqlite3.
+        'USER': 'adidas',                      # Not used with sqlite3.
+        'PASSWORD': 'g2dEyuba',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'VERSION': '1',
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = '/var/tmp/dummy_email' # change this to a proper location
+# Keys from user 'django'
+AWS_ACCESS_KEY_ID = 'AKIAIC6VFIJCTGUNGU4A'
+AWS_SECRET_ACCESS_KEY = '19PkS5VMoy5V2E2UHp7uxYGfWzfzctX/97noMG99'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+AWS_STORAGE_BUCKET_NAME = 'pictureme_bucket'
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/pictureme_bucket'
+#AWS_S3_CUSTOM_DOMAIN = 'd2579uvf4z4cis.cloudfront.net'
+#AWS_CF_DISTRIBUTION_ID = 'E209JJ6DBAW14K'
+
+
+# There is no MEDIA URL/ROOT or STATIC_ROOT because is using a different Storage
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATIC_URL = 'http://%s/static/' % 's3.amazonaws.com/pictureme_bucket'
+
+ADMIN_MEDIA_PREFIX = 'http://%s/admin/' % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_DIRS = (
+    ("static", "/home/ubuntu/projects/picturme/docroot/static"),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True
         }
     },
     'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
+            'propagate': True,
+        },
+        'adidas': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     }
---
+++ source/fivehundred.py
@@ -0,0 +1,74 @@
+
+
+import urllib
+
+# Find a JSON parser
+try:
+    import json
+    _parse_json = lambda s: json.loads(s)
+except ImportError:
+    try:
+        import simplejson
+        _parse_json = lambda s: simplejson.loads(s)
+    except ImportError:
+        # For Google AppEngine
+        from django.utils import simplejson
+        _parse_json = lambda s: simplejson.loads(s)
+        
+
+class FiveHundredPx:
+    BASE_URL = 'https://api.500px.com/v1'
+    
+    def __init__(self,consumer_key):
+        self.consumer_key = consumer_key
+
+    def request(self, path, args=None, post_args=None):
+        """Fetches the given path in the Graph API.
+
+        We translate args to a valid query string. If post_args is given,
+        we send a POST request to the given path with the given arguments.
+        """
+        if not args: args = {}
+        if self.consumer_key:
+            if post_args is not None:
+                post_args["consumer_key"] = self.consumer_key
+            else:
+                args["consumer_key"] = self.consumer_key
+        
+        post_data = None if post_args is None else urllib.urlencode(post_args)
+        
+        try:
+            
+            print 'log path: %s' % (FiveHundredPx.BASE_URL + path + "?" + urllib.urlencode(args))
+            file_resp = urllib.urlopen(FiveHundredPx.BASE_URL + path + "?" +
+                              urllib.urlencode(args), post_data)
+        
+            response = _parse_json(file_resp.read())
+        finally:
+            file_resp.close()
+        if response.get("error"):
+            pass
+            #raise APIError(response["error"], response["status"])
+            
+        return response
+
+    def get_photos(self,feature='editors'):
+        args = {"feature": feature}
+        
+        data = self.request('/photos', args)
+        total_pages = data['total_pages']
+        
+        page = data['current_page']
+       
+        while page <= total_pages:
+        
+            for p in data['photos']:
+                yield p
+            
+            args['page'] = page = page+1
+            data = self.request('/photos', args)
+        
+        
+            
+            
+            
