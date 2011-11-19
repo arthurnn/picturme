76ba60522bc970c6cc2b106bbd83e79aac95b340
Olivier <olivier.tille@gmail.com>
2011-11-19 16:34:30 -0500

    added loading template
    template to display that the image is being processed

--- source/urls.py
+++ source/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'index.html'}, name='home'),
-    url(r'^detail/$', direct_to_template, {'template': 'details.html'}, name='home'),
+    url(r'^detail/$', direct_to_template, {'template': 'details.html'}, name='detail'),
+    url(r'^loading/$', direct_to_template, {'template': 'loading.html'}, name='loading'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
