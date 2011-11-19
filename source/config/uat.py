73b9cab292e4a980b1d7c4c83bb9173583b632d7
Arthur Neves <arthurnn@gmail.com>
2011-11-18 22:23:14 -0500

    a

--- source/config/uat.py
+++ source/config/uat.py
@@ -10,17 +10,13 @@ PROJECT_ROOT = abspath(join(dirname(__file__), "../"))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ANALYTICS_ID = 'UA-25908866-2'
-RECAPTCHA_PUB_KEY = "6LfA08gSAAAAALDg2e5xIbCujNbNDFD5EATGYGpr"
-RECAPTCHA_PRIVATE_KEY = "6LfA08gSAAAAALObXgOJg97Aye6lU93bemuh8lmS"
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'adidaslookbook',                      # Or path to database file if using sqlite3.
-        'USER': 'adidas',                      # Not used with sqlite3.
-        'PASSWORD': 'g2dEyuba',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'picturme',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'uik9BrYBXkd',                  # Not used with sqlite3.
+        'HOST': '50.19.93.10',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
