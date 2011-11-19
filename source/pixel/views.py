18a597ddc7be8bd4d3c67f52452c9295c3666758
Arthur Neves <arthurnn@gmail.com>
2011-11-19 18:12:26 -0500

    parm

--- source/pixel/views.py
+++ source/pixel/views.py
@@ -28,7 +28,7 @@ def upload(request):
         imgFile = Image.open(StringIO(ff.read()))
         
         ap = ImageTrans()
-        mosaic = ap.main(imgFile,10)
+        mosaic = ap.main(imgFile,4)
         
         photo = UserImage()
         sio = StringIO()
