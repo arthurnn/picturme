716b4cffb35bb98c7471beae482824fe72c87860
Arthur Neves <arthurnn@gmail.com>
2011-11-22 17:10:44 -0500

    add ajax to read image from base64

--- source/pixel/admin.py
+++ source/pixel/admin.py
@@ -4,6 +4,7 @@ Created on Aug 4, 2011
 @author: arthurnn
 '''
 from django.contrib import admin
-from models import Pixel
+from models import Pixel,UserImage
 
 admin.site.register(Pixel)
+admin.site.register(UserImage)
--- source/pixel/views.py
+++ source/pixel/views.py
@@ -7,7 +7,8 @@ from django.db.models import Count
 from django.template import RequestContext
 from django.db import connection
 from django.utils.translation import ugettext as _
-from annoying.decorators import render_to
+from annoying.decorators import render_to,ajax_request
+from django.views.decorators.csrf import csrf_exempt
 from django.core.files.base import ContentFile
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -20,7 +21,7 @@ from pixel.image2 import ImageTrans
 
 from pixel.models import UserImage,UserTiles
 
-import hashlib, short_url
+import hashlib, short_url, base64
 
 @render_to('index.html')
 def home(request):
@@ -29,50 +30,63 @@ def home(request):
     return {'images':ii}
     
 
+def handleImage(imgFile):
+    imgFile = imgFile.convert('RGB')
+    
+    ap = ImageTrans()
+    #imgFile.thumbnail((500,500))
+    (mosaic,arr) = ap.main(imgFile)
+    
+    photo = UserImage()
+    sio = StringIO()
+    mosaic.save(sio,'JPEG')
+    
+    fn_image = hashlib.md5(sio.getvalue()).hexdigest()+'.jpg'
+    photo.image.save(fn_image, ContentFile(sio.getvalue()), save=False)
+    
+    
+    m_copy = mosaic.copy()
+    m_copy.thumbnail((500,500))
+    sio = StringIO()
+    m_copy.save(sio,'JPEG')
+    
+    fn_image = hashlib.md5(sio.getvalue()).hexdigest()+'.jpg'
+    photo.thumbnail.save(fn_image, ContentFile(sio.getvalue()), save=False)
+    
+    photo.save()
+    
+    for (pixel_id, xy_list) in arr.iteritems():
+            tt = UserTiles(user_image=photo)
+            tt.pixel_id = pixel_id
+            tt.save()
+            
+    return photo
+    
+
+@ajax_request
+@csrf_exempt
+def mobileUpload(request):
+    ff = request.POST.get('file',False)
+    if ff:
+        s = base64.decodestring(ff)
+        imgFile = Image.open(StringIO(s))
+        photo = handleImage(imgFile)
+        
+        url = '/d/%s' % short_url.encode_url(photo.id)
+        return {'success':True, 'url':url}
+    
+
 def upload(request):
     
     ff = request.FILES.get('file',False)
     if ff:
         imgFile = Image.open(StringIO(ff.read()))
-        imgFile = imgFile.convert('RGB')
-
         
-        ap = ImageTrans()
-        #imgFile.thumbnail((500,500))
-        (mosaic,arr) = ap.main(imgFile)
-        
-        photo = UserImage()
-        sio = StringIO()
-        mosaic.save(sio,'JPEG')
-        
-        fn_image = hashlib.md5(sio.getvalue()).hexdigest()+'.jpg'
-        photo.image.save(fn_image, ContentFile(sio.getvalue()), save=False)
-        
-        
-        m_copy = mosaic.copy()
-        m_copy.thumbnail((500,500))
-        sio = StringIO()
-        m_copy.save(sio,'JPEG')
-        
-        fn_image = hashlib.md5(sio.getvalue()).hexdigest()+'.jpg'
-        photo.thumbnail.save(fn_image, ContentFile(sio.getvalue()), save=False)
-        
-        photo.save()
-        
-        
-        for (pixel_id, xy_list) in arr.iteritems():
-            tt = UserTiles(user_image=photo)
-            tt.pixel_id = pixel_id
-            tt.save()
-            
-        
-#        for a in arr:
-#            a.user_image=photo
-#            a.save()
+        photo = handleImage(imgFile)
+        url = short_url.encode_url(photo.id)
+    else:
+        url = 'sorry'
     
-    
-    url = short_url.encode_url(photo.id)
-        
     return redirect('/d/%s'%url)
 
 
