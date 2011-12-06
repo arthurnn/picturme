29ef1362bf142f663aa19416ba1212ffec19c709
Arthur Neves <arthurnn@gmail.com>
2011-12-05 23:54:03 -0500

    nn

--- source/pixel/image2.py
+++ source/pixel/image2.py
@@ -76,12 +76,11 @@ def top_down(grid, output, tile_size):
             #tile = image_list.search(rgb).image.blob
             #tile_wrapper = image_list.search(rgb)
             
-            t = random.randint(1,4)
-            w = image_list.query(query_point=qrgb, t=t)
+            w = image_list.query(query_point=qrgb, t=1)
             
-            i = random.randint(0,len(w)-1)
+            #i = random.randint(0,len(w)-1)
             
-            tile_pixel = w[i]
+            tile_pixel = w[0]
             
             #tile_pixel = tile_wrapper.pixel
             #print tile_pixel.id
